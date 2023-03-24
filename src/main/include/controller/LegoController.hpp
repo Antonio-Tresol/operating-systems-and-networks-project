4022af8241d11b2f287fc77ec27d9318884a4234
Ariel Arévalo Alvarado <71473451+arielarevalo@users.noreply.github.com>
2023-03-23 21:22:29 -0600

    Agrega estructura del cliente.

---
+++ src/main/include/controller/LegoController.hpp
@@ -0,0 +1,4 @@
+// Copyright 2023 Ariel Arevalo Alvarado <ariel.arevalo@ucr.ac.cr>.
+// Copyright 2023 Antonio Badilla Olivas <anthonny.badilla@ucr.ac.cr>.
+
+#pragma once
---
+++ src/main/include/network/Socket.hpp
@@ -0,0 +1,8 @@
+// Copyright 2023 Ariel Arevalo Alvarado <ariel.arevalo@ucr.ac.cr>.
+// Copyright 2023 Antonio Badilla Olivas <anthonny.badilla@ucr.ac.cr>.
+
+#pragma once
+
+/**
+* Clase de trabajo en clase sin layer mixing
+*/
