370a79c6b69532087561b2393e2790e32d0bc0fa
Geancarlo Rivera <geancarlo.riverahernandez@ucr.ac.cr>
2023-03-30 15:20:17 -0600

    Agrega FigureRepository.cpp con el método para obtener el html de la figura (codigo comentado)

--- src/main/include/data/FigureRepository.hpp
+++ src/main/include/data/FigureRepository.hpp
@@ -1,5 +1,7 @@
 // Copyright 2023 Ariel Arevalo Alvarado <ariel.arevalo@ucr.ac.cr>.
 // Copyright 2023 Antonio Badilla Olivas <anthonny.badilla@ucr.ac.cr>.
+// Copyright 2023 Geancarlo Rivera Hernandez
+// <geancarlo.riverahernandez@ucr.ac.cr>.
 
 #pragma once
 
@@ -7,10 +9,14 @@
 
 #include "model/Figure.hpp"
 #include "net/HttpClient.hpp"
+#include "net/Socket.hpp"
+
+#define SIZE 1024
 
 class FigureRepository {
  public:
   Figure findByName(std::string name);
  private:
-  static constexpr char FIGURE_URL[]{"os.ecci.ucr.ac.cr/lego/list.php?figure="};
+  static constexpr char HOST[]{"os.ecci.ucr.ac.cr"};
+  static constexpr char FIGURE_URL[]{"GET /lego/list.php?figure="};
 };
