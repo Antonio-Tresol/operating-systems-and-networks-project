fe520e92c2097dc5f92e11f6bdfe06fceb20f3c7
Antonio Badilla-Olivas <anthonny.badilla@ucr.ac.cr>
2023-05-06 14:37:09 -0600

    Updating FigureHttpsServer header - Adding client queue, handlers (std::thread) vector. - changing the name of member variable Ipv4SslSocket to server. -adding private method signatures.

--- lego-server/main/include/net/FigureHttpsServer.hpp
+++ lego-server/main/include/net/FigureHttpsServer.hpp
@@ -1,46 +1,59 @@
 // Copyright 2023 Ariel Arevalo Alvarado <ariel.arevalo@ucr.ac.cr>.
 // Copyright 2023 Antonio Badilla Olivas <anthonny.badilla@ucr.ac.cr>.
 // Copyright 2023 Jean Paul Chacon Gonzalez <jean.chacongonzalez@ucr.ac.cr>.
-// Copyright 2023 Geancarlo Rivera Hernandez <geancarlo.riverahernandez@ucr.ac.cr>.
+// Copyright 2023 Geancarlo Rivera Hernandez
+// <geancarlo.riverahernandez@ucr.ac.cr>.
 
 #pragma once
 
 #include <functional>
-#include <string>
-#include <sstream>
 #include <map>
 #include <regex>
+#include <sstream>
+#include <string>
 #include <thread>
+#include <vector>
 
-#include "./socket/Ipv4SslSocket.hpp"
+#include "../common/Queue.hpp"
 #include "../controller/FigureController.hpp"
+#include "./socket/Ipv4SslSocket.hpp"
 
 // using Handler = std::function<void(const std::string&)>;
-using Handler = std::jthread;
+using Handler = std::thread;
 /**
  * @brief Makes HTTP requests.
  */
 class FigureHttpsServer {
  public:
-  FigureHttpsServer();
+  FigureHttpsServer(std::string certificatesFilePath, int port = 7777);
   /**
    * @brief Makes a GET request to the given resource and host.
    * @param resource Resource to request.
    * @param host Host to request from.
    * @return Response from host.
    */
-  [[nodiscard]] std::string get(const std::string &host, 
-    const std::string &resource) const;
+  [[nodiscard]] std::string get(const std::string& host,
+                                const std::string& resource) const;
+
+  void start();
+  void stop();
 
  private:
   static constexpr char GET[]{"GET "};
   static constexpr char CRLF[]{"\r\n"};
   static constexpr char HOST[]{"Host: "};
   static constexpr char HTTPS[]{"https"};
-  Ipv4SslSocket socket{};
+  Ipv4SslSocket server{};
   FigureController figureController{};
-
-  std::string generateHttpResponse(int statusCode, std::string& headers, 
-    const std::string& body);
-
+  Queue<Ipv4SslSocket*> clientQueue{};
+  std::vector<Handler*> handlers{};
+  void handleRequests();
+  std::map<std::string, std::string> getUrlParams(
+      const std::string& httpRequest);
+  std::string FigureHttpsServer::generateHttpResponse(
+      int statusCode, const std::map<std::string, std::string>& headers,
+      const std::string& body);
+  void sendHttpResponse(Ipv4SslSocket* client, int statusCode,
+                        const std::map<std::string, std::string>& headers,
+                        const std::string& body);
 };
