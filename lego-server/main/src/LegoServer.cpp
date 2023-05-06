31abb949653786223d5c737293a9be742d757b56
Antonio Badilla-Olivas <anthonny.badilla@ucr.ac.cr>
2023-05-06 14:40:24 -0600

    LegoServer.cpp: adding signal handler - to stop gracefully "stop" in FigureHttpsServer must be implemented.

--- lego-server/main/src/LegoServer.cpp
+++ lego-server/main/src/LegoServer.cpp
@@ -1,8 +1,9 @@
 // Copyright 2023 Ariel Arevalo Alvarado <ariel.arevalo@ucr.ac.cr>.
 // Copyright 2023 Antonio Badilla Olivas <anthonny.badilla@ucr.ac.cr>.
 // Copyright 2023 Jean Paul Chacon Gonzalez <jean.chacongonzalez@ucr.ac.cr>.
-// Copyright 2023 Geancarlo Rivera Hernandez <geancarlo.riverahernandez@ucr.ac.cr>.
-
+// Copyright 2023 Geancarlo Rivera Hernandez
+// <geancarlo.riverahernandez@ucr.ac.cr>.
+#include <csignal>
 #include <string>
 
 #include "../include/common/Logger.hpp"
@@ -14,20 +15,37 @@ using std::out_of_range;
 using std::stoi;
 using std::string;
 
+void signalAction(int signum);
+void signalHandle();
+FigureHttpsServer *serverPtr = nullptr;
 /**
  * @brief Entry point.
  */
 int main(int argc, char *argv[]) {
-    Logger::initialize();
-    try {
-      FigureHttpsServer().start();
-    }
-    catch (exception const &e) {
-        Logger::error("Server has crashed.", e);
-        exit(1);
-    }
+  Logger::initialize();
+  try {
+    FigureHttpsServer server("certificates");
+    serverPtr = &server;
+    signalHandle();
+    server.start();
+  } catch (exception const &e) {
+    Logger::error("Server has crashed.", e);
+    exit(1);
+  }
 
-    Logger::info("Exiting.");
+  Logger::info("Exiting.");
 
-    exit(0);
+  exit(0);
+}
+
+void signalAction(int signum) {
+  if (serverPtr != nullptr) {
+    serverPtr->stop();
+  }
+  exit(signum);
+}
+
+void signalHandle() {
+  std::signal(SIGINT, signalAction);
+  std::signal(SIGTERM, signalAction);
 }
