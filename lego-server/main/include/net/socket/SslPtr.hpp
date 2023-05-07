0f0fc79f74feecdae5f2fd7f0999bde682c89178
Antonio Badilla-Olivas <anthonny.badilla@ucr.ac.cr>
2023-05-06 22:29:48 -0600

    adding ssl secure pointers updating  (not stable)

--- lego-server/main/include/net/socket/SslPtr.hpp
+++ lego-server/main/include/net/socket/SslPtr.hpp
@@ -1,12 +1,14 @@
 // Copyright 2023 Ariel Arevalo Alvarado <ariel.arevalo@ucr.ac.cr>.
 // Copyright 2023 Antonio Badilla Olivas <anthonny.badilla@ucr.ac.cr>.
 // Copyright 2023 Jean Paul Chacon Gonzalez <jean.chacongonzalez@ucr.ac.cr>.
-// Copyright 2023 Geancarlo Rivera Hernandez <geancarlo.riverahernandez@ucr.ac.cr>.
+// Copyright 2023 Geancarlo Rivera Hernandez
+// <geancarlo.riverahernandez@ucr.ac.cr>.
 
 #pragma once
 
-#include <openssl/ssl.h>
 #include <openssl/err.h>
+#include <openssl/ssl.h>
+
 #include <iostream>
 #include <stdexcept>
 
--- lego-server/main/src/net/socket/SslCtxPtr.cpp
+++ lego-server/main/src/net/socket/SslCtxPtr.cpp
@@ -1,29 +1,25 @@
 // Copyright 2023 Ariel Arevalo Alvarado <ariel.arevalo@ucr.ac.cr>.
 // Copyright 2023 Antonio Badilla Olivas <anthonny.badilla@ucr.ac.cr>.
 // Copyright 2023 Jean Paul Chacon Gonzalez <jean.chacongonzalez@ucr.ac.cr>.
-// Copyright 2023 Geancarlo Rivera Hernandez <geancarlo.riverahernandez@ucr.ac.cr>.
+// Copyright 2023 Geancarlo Rivera Hernandez
+// <geancarlo.riverahernandez@ucr.ac.cr>.
 
 #include "../include/net/socket/SslCtxPtr.hpp"
 
 using std::runtime_error;
 
 SslCtxPtr::SslCtxPtr() {
-    ctx = SSL_CTX_new(TLS_client_method());
-    if (!ctx) {
-        throw runtime_error("Failed to create SSL_CTX");
-    }
+  ctx = SSL_CTX_new(TLS_client_method());
+  if (!ctx) {
+    throw runtime_error("Failed to create SSL_CTX");
+  }
 }
-void SslCtxPtr::setCtx(SSL_CTX *ctx){
-    this->ctx = ctx;
-}
+void SslCtxPtr::setCtx(SSL_CTX *ctx) { this->ctx = ctx; }
 
-SslCtxPtr::SslCtxPtr() noexcept {
-}
+SslCtxPtr::SslCtxPtr() noexcept {}
 
 SslCtxPtr::~SslCtxPtr() {
-    if (ctx) {
-        SSL_CTX_free(ctx);
-    }
+  if (ctx) {
+    SSL_CTX_free(ctx);
+  }
 }
-
-
