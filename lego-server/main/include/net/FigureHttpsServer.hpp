813b387d38f5cff8ee6596841342522e675e3336
Antonio Badilla-Olivas <anthonny.badilla@ucr.ac.cr>
2023-05-17 16:09:16 -0600

    handling request from client

--- lego-server/main/include/net/FigureHttpsServer.hpp
+++ lego-server/main/include/net/FigureHttpsServer.hpp
@@ -1,14 +1,15 @@
 // Copyright 2023 Ariel Arevalo Alvarado <ariel.arevalo@ucr.ac.cr>.
 // Copyright 2023 Antonio Badilla Olivas <anthonny.badilla@ucr.ac.cr>.
 // Copyright 2023 Jean Paul Chacon Gonzalez <jean.chacongonzalez@ucr.ac.cr>.
-// Copyright 2023 Geancarlo Rivera Hernandez <geancarlo.riverahernandez@ucr.ac.cr>.
+// Copyright 2023 Geancarlo Rivera Hernandez
+// <geancarlo.riverahernandez@ucr.ac.cr>.
 
 #pragma once
 
 #include <functional>
 #include <map>
-#include <regex>
 #include <memory>
+#include <regex>
 #include <sstream>
 #include <string>
 #include <thread>
@@ -24,40 +25,42 @@ using Worker = std::thread;
  * @brief Makes HTTP requests.
  */
 class FigureHttpsServer {
-public:
-    ~FigureHttpsServer();
+ public:
+  ~FigureHttpsServer();
 
-    [[noreturn]] void start();
+  [[noreturn]] void start();
 
-    void stop();
+  void stop();
 
-private:
-    static constexpr int PORT{7777};
-    static constexpr int QUEUE{128};
-    static constexpr char CERT_PATH[]{"./ci0123.pem"};
-    static constexpr char FIGURE[]{"figure"};
+ private:
+  static constexpr int PORT{7777};
+  static constexpr int QUEUE{128};
+  static constexpr char CERT_PATH[]{"./ci0123.pem"};
+  static constexpr char FIGURE[]{"figure"};
 
-    IPv4SslSocket listener{CERT_PATH, CERT_PATH};
+  IPv4SslSocket listener{CERT_PATH, CERT_PATH};
 
-    FigureController figureController{};
+  FigureController figureController{};
 
-    Queue<std::shared_ptr<IPv4SslSocket>> clientQueue{};
+  Queue<std::shared_ptr<IPv4SslSocket>> clientQueue{};
 
-    std::vector<Worker> workers{};
+  std::vector<Worker> workers{};
 
-    void handleRequests();
+  void handleRequests();
 
-    std::map<std::string, std::map<std::string, std::string>> parseHttpRequest(const std::string& request);
+  std::map<std::string, std::string> parseHeaders(std::istringstream &stream);
 
-    std::map<std::string, std::string> parseHeaders(std::istringstream& stream);
+  std::map<std::string, std::map<std::string, std::string>> parseHttpRequest(
+      const std::string &request);
 
-    static std::string getLastPath(const std::string &url);
+  static std::string getLastPath(const std::string &url);
 
-    static std::string generateHttpResponse(
-            int statusCode, const std::map<std::string, std::string> &headers,
-            const std::string &body);
+  static std::string generateHttpResponse(
+      int statusCode, const std::map<std::string, std::string> &headers,
+      const std::string &body);
 
-    static void sendHttpResponse(const std::shared_ptr<IPv4SslSocket>& client, int statusCode,
-                                 const std::map<std::string, std::string> &headers,
-                                 const std::string &body);
+  static void sendHttpResponse(
+      const std::shared_ptr<IPv4SslSocket> &client, int statusCode,
+      const std::map<std::string, std::string> &headers,
+      const std::string &body);
 };
--- lego-server/main/src/net/FigureHttpsServer.cpp
+++ lego-server/main/src/net/FigureHttpsServer.cpp
@@ -1,47 +1,45 @@
 // Copyright 2023 Ariel Arevalo Alvarado <ariel.arevalo@ucr.ac.cr>.
 // Copyright 2023 Antonio Badilla Olivas <anthonny.badilla@ucr.ac.cr>.
 // Copyright 2023 Jean Paul Chacon Gonzalez <jean.chacongonzalez@ucr.ac.cr>.
-// Copyright 2023 Geancarlo Rivera Hernandez <geancarlo.riverahernandez@ucr.ac.cr>.
+// Copyright 2023 Geancarlo Rivera Hernandez
+// <geancarlo.riverahernandez@ucr.ac.cr>.
 
 #include "../include/net/FigureHttpsServer.hpp"
 
 using ::Worker;
 
 using std::exception;
+using std::istringstream;
 using std::map;
 using std::regex;
 using std::runtime_error;
 using std::smatch;
 using std::sregex_iterator;
 using std::string;
-using std::istringstream;
 using std::to_string;
 
-
-FigureHttpsServer::~FigureHttpsServer() {
-    stop();
-}
+FigureHttpsServer::~FigureHttpsServer() { stop(); }
 
 /**
- * @brief Starts the listener, creates the threads and starts them running. Besides
- * accepts incoming connections and puts them in the client queue.
+ * @brief Starts the listener, creates the threads and starts them running.
+ * Besides accepts incoming connections and puts them in the client queue.
  */
 void FigureHttpsServer::start() {
-    listener.bind(PORT);
+  listener.bind(PORT);
 
-    listener.listen(QUEUE);
+  listener.listen(QUEUE);
 
-    for (int i{0}; i < 4; ++i) {
-        this->workers.emplace_back(&FigureHttpsServer::handleRequests, this);
-    }
+  for (int i{0}; i < 4; ++i) {
+    this->workers.emplace_back(&FigureHttpsServer::handleRequests, this);
+  }
 
-    Logger::info("Listener certificates: \n" + listener.getCerts());
+  Logger::info("Listener certificates: \n" + listener.getCerts());
 
-    Logger::info("Listening...");
+  Logger::info("Listening...");
 
-    while (true) {
-        this->clientQueue.enqueue(listener.accept());
-    }
+  while (true) {
+    this->clientQueue.enqueue(listener.accept());
+  }
 }
 
 /**
@@ -50,107 +48,113 @@ void FigureHttpsServer::start() {
  * stop and join the threads
  */
 void FigureHttpsServer::stop() {
-    for (auto &worker: workers) {
-        clientQueue.enqueue(nullptr);
-    }
+  for (auto &worker : workers) {
+    clientQueue.enqueue(nullptr);
+  }
 
-    for (auto &handler: workers) {
-        handler.join();
-    }
+  for (auto &handler : workers) {
+    handler.join();
+  }
 }
 
 void FigureHttpsServer::handleRequests() {
-    while (true) {
-        auto client{clientQueue.dequeue()};
-        if (!client) {
-            break;
-        } else {
-            client->sslAccept();
+  while (true) {
+    auto client{clientQueue.dequeue()};
+    if (!client) {
+      break;
+    } else {
+      client->sslAccept();
 
-            Logger::info(client->getCerts());
+      Logger::info(client->getCerts());
 
-            string request{client->sslRead()};
+      string request{client->sslRead()};
 
-            // parseHttpRequest for all bits of request
+      // parseHttpRequest for all bits of request
+      map<string, map<string, string>> parsedRequest{parseHttpRequest(request)};
+      map<string, string> headers{parsedRequest["Headers"]};
+      // isolate url, getLastPath, pass to getFigureByName
+      string url{parsedRequest["Request-Line"]["URL"]};
+      // isolate headers, pass to sendHttpResponse
+      string body{figureController.getFigureByName(getLastPath(url))};
 
-            // isolate url, getLastPath, pass to getFigureByName
-
-            // isolate headers, pass to sendHttpResponse
-
-            string body{figureController.getFigureByName(params[FIGURE])};
-
-            if (body.empty()) {
-                sendHttpResponse(client, 404, params, body);
-            } else {
-                sendHttpResponse(client, 200, params, body);
-            }
-        }
+      if (body.empty()) {
+        sendHttpResponse(client, 404, headers, body);
+      } else {
+        sendHttpResponse(client, 200, headers, body);
+      }
     }
+  }
 }
 
-map<string, map<string, string>> parseHttpRequest(const string& request) {
-    istringstream requestStream(request);
-    string method, url, version;
-    requestStream >> method >> url >> version;
+map<string, map<string, string>> FigureHttpsServer::parseHttpRequest(
+    const string &request) {
+  istringstream requestStream(request);
+  string method, url, version;
+  requestStream >> method >> url >> version;
 
-    map<string, map<string, string>> result{
-            {"Request-Line", {{"Method", method}, {"URL", url}, {"Version", version}}},
-            {"Headers", parseHeaders(requestStream)}
-    };
+  map<string, map<string, string>> result{
+      {"Request-Line",
+       {{"Method", method}, {"URL", url}, {"Version", version}}},
+      {"Headers", parseHeaders(requestStream)}};
 
-    return result;
+  return result;
 }
 
-map<string, string> parseHeaders(istringstream& stream) {
-    map<string, string> headers;
-    string line;
-    while (getline(stream, line) && line != "\r") {
-        auto colonPos = line.find(':');
-        if (colonPos != string::npos) {
-            string name = line.substr(0, colonPos);
-            string value = line.substr(colonPos + 2); // Skip the colon and the space after it
-            headers[name] = value.substr(0, value.size() - 1); // Remove the trailing '\r'
-        }
+map<string, string> FigureHttpsServer::parseHeaders(istringstream &stream) {
+  map<string, string> headers;
+  string line;
+  while (getline(stream, line) && line != "\r") {
+    auto colonPos = line.find(':');
+    if (colonPos != string::npos) {
+      string name = line.substr(0, colonPos);
+      string value =
+          line.substr(colonPos + 2);  // Skip the colon and the space after it
+      headers[name] =
+          value.substr(0, value.size() - 1);  // Remove the trailing '\r'
     }
-    return headers;
+  }
+  return headers;
 }
 
 string FigureHttpsServer::getLastPath(const string &url) {
-    size_t pos = url.find_last_of('/');
-    if (pos != string::npos) {
-        return url.substr(pos + 1); // Return the part after the last '/'
-    }
-    return url; // Return the whole URL if there is no '/'
+  size_t pos = url.find_last_of('/');
+  if (pos != string::npos) {
+    return url.substr(pos + 1);  // Return the part after the last '/'
+  }
+  return url;  // Return the whole URL if there is no '/'
 }
 
-string FigureHttpsServer::generateHttpResponse(int statusCode, const map<string, string> &headers,
-                                               const string &body) {
-    string statusMessage;
-    switch (statusCode) {
-        case 200:
-            statusMessage = "OK";
-            break;
-        case 404:
-            statusMessage = "Not Found";
-            break;
-        default:
-            statusMessage = "Unknown";
-            break;
-    }
+string FigureHttpsServer::generateHttpResponse(
+    int statusCode, const map<string, string> &headers, const string &body) {
+  string statusMessage;
+  switch (statusCode) {
+    case 200:
+      statusMessage = "OK";
+      break;
+    case 404:
+      statusMessage = "Not Found";
+      break;
+    default:
+      statusMessage = "Unknown";
+      break;
+  }
 
-    string response = "HTTP/1.1 " + to_string(statusCode) + " " + statusMessage + "\r\n";
-    for (const auto &header: headers) {
-        response += header.first + ": " + header.second + "\r\n";
-    }
-    response += "Content-Length: " + to_string(body.size()) + "\r\n";
-    response += "\r\n" + body;
+  string response =
+      "HTTP/1.1 " + to_string(statusCode) + " " + statusMessage + "\r\n";
+  for (const auto &header : headers) {
+    response += header.first + ": " + header.second + "\r\n";
+  }
+  response += "Content-Length: " + to_string(body.size()) + "\r\n";
+  response += "\r\n" + body;
 
-    return response;
+  return response;
 }
 
-void FigureHttpsServer::sendHttpResponse(const std::shared_ptr<IPv4SslSocket>& client, int statusCode,
-                                         const std::map<std::string, std::string> &headers,
-                                         const std::string &body) {
-    string response = FigureHttpsServer::generateHttpResponse(statusCode, headers, body);
-    client->sslWrite(response);
+void FigureHttpsServer::sendHttpResponse(
+    const std::shared_ptr<IPv4SslSocket> &client, int statusCode,
+    const std::map<std::string, std::string> &headers,
+    const std::string &body) {
+  string response =
+      FigureHttpsServer::generateHttpResponse(statusCode, headers, body);
+  client->sslWrite(response);
 }
