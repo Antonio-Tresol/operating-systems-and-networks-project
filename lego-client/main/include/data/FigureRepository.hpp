cbd24ca90ea7de6a0cd288f8a10f2be166fa9f87
Antonio Badilla Olivas <anthonny.badilla@ucr.ac.cr>
2023-05-19 22:51:58 -0600

    fixing typo on client

--- lego-client/main/include/data/FigureRepository.hpp
+++ lego-client/main/include/data/FigureRepository.hpp
@@ -1,7 +1,8 @@
 // Copyright 2023 Ariel Arevalo Alvarado <ariel.arevalo@ucr.ac.cr>.
 // Copyright 2023 Antonio Badilla Olivas <anthonny.badilla@ucr.ac.cr>.
 // Copyright 2023 Jean Paul Chacon Gonzalez <jean.chacongonzalez@ucr.ac.cr>.
-// Copyright 2023 Geancarlo Rivera Hernandez <geancarlo.riverahernandez@ucr.ac.cr>.
+// Copyright 2023 Geancarlo Rivera Hernandez
+// <geancarlo.riverahernandez@ucr.ac.cr>.
 
 #pragma once
 
@@ -24,7 +25,7 @@ class FigureRepository {
 
  private:
   static constexpr char HOST[]{"127.0.0.1"};
-  static constexpr char URL_TEMPLATE[]{"/figure/"};
+  static constexpr char URL_TEMPLATE[]{"/lego/"};
 
   HttpsClient httpsClient{};
 };
