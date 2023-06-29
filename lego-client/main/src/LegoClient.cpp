a245fba523ec50bda1ca73b8123bc5023ac6feff
Antonio Badilla-Olivas <anthonny.badilla@ucr.ac.cr>
2023-06-28 21:22:10 -0600

    adding abstract class ssl server

--- lego-client/main/src/LegoClient.cpp
+++ lego-client/main/src/LegoClient.cpp
@@ -1,11 +1,12 @@
 // Copyright 2023 Ariel Arevalo Alvarado <ariel.arevalo@ucr.ac.cr>.
 // Copyright 2023 Antonio Badilla Olivas <anthonny.badilla@ucr.ac.cr>.
 // Copyright 2023 Jean Paul Chacon Gonzalez <jean.chacongonzalez@ucr.ac.cr>.
-// Copyright 2023 Geancarlo Rivera Hernandez <geancarlo.riverahernandez@ucr.ac.cr>.
+// Copyright 2023 Geancarlo Rivera Hernandez
+// <geancarlo.riverahernandez@ucr.ac.cr>.
 
 #include <string>
 
-#include "./common/Logger.hpp"
+#include "../../common/Logger.hpp"
 #include "./controller/FigureController.hpp"
 
 using std::exception;
@@ -18,25 +19,24 @@ using std::string;
  * @brief Entry point.
  */
 int main(int argc, char *argv[]) {
-    Logger::initialize();
-    try {
-        if (argc < 2) {
-            Logger::error("Invalid arguments: No figure name provided.");
+  Logger::initialize();
+  try {
+    if (argc < 2) {
+      Logger::error("Invalid arguments: No figure name provided.");
 
-            exit(1);
-        }
-
-        string figureName{argv[1]};
-
-        FigureController().printFigureByName(figureName);
-    }
-    catch (exception const &e) {
-        Logger::error("Client has crashed.", e);
-
-        exit(1);
+      exit(1);
     }
 
-    Logger::info("Finished.");
+    string figureName{argv[1]};
 
-    exit(0);
+    FigureController().printFigureByName(figureName);
+  } catch (exception const &e) {
+    Logger::error("Client has crashed.", e);
+
+    exit(1);
+  }
+
+  Logger::info("Finished.");
+
+  exit(0);
 }
--- lego-common/main/include/net/SslServer.hpp
+++ lego-common/main/include/net/SslServer.hpp
@@ -1,14 +1,42 @@
 //
 // Created by ariel on 6/24/23.
 //
-
 #ifndef PI_REDES_SISTEMAS_OPERATIVOS_SSLSERVER_HPP
 #define PI_REDES_SISTEMAS_OPERATIVOS_SSLSERVER_HPP
 
+#include <map>
+#include <memory>
+#include <thread>
+#include <vector>
 
+using Worker = std::thread;
+
+#include "./concurrency/Queue.hpp"
+#include "./socket/IPv4SslSocket.hpp"
 class SslServer {
+ public:
+  explicit SslServer(int32_t numWorkers, const std::string& certPath,
+                     int32_t port);
 
+  virtual ~SslServer();
+
+  [[noreturn]] void start();
+  void stop();
+
+ protected:
+  const std::string CERT_PATH;
+  const int32_t NUM_WORKERS;
+  const int32_t PORT;
+  static constexpr int QUEUE{128};
+
+  IPv4SslSocket listener;
+  Queue<std::shared_ptr<IPv4SslSocket>> clientQueue{};
+  std::vector<Worker> workers{};
+
+  virtual void handleClient(const std::shared_ptr<IPv4SslSocket>& client) = 0;
+
+ private:
+  void handleRequests();
 };
 
-
-#endif //PI_REDES_SISTEMAS_OPERATIVOS_SSLSERVER_HPP
+#endif  // PI_REDES_SISTEMAS_OPERATIVOS_SSLSERVER_HPP
