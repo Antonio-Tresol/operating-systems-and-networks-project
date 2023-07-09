0cd8995b3ea5289a5dbe4c9f7325ee6c6b133b87
Antonio Badilla-Olivas <anthonny.badilla@ucr.ac.cr>
2023-07-08 20:28:36 -0600

    passing functions from proxy https controller to proxy https server.

--- lego-proxy/main/include/controller/ProxyHttpsController.hpp
+++ lego-proxy/main/include/controller/ProxyHttpsController.hpp
@@ -1,76 +1,18 @@
 #pragma once
 
-#include "../../../../lego-common/main/include/net/socket/IPv4SslSocket.hpp"
-#include "../../../../lego-common/main/include/logging/Logger.hpp"
 #include <map>
-#include <string>
 #include <sstream>
+#include <string>
+
+#include "../../../../lego-common/main/include/logging/Logger.hpp"
+#include "../../../../lego-common/main/include/net/socket/IPv4SslSocket.hpp"
 
 class ProxyHttpsController {
-  public:
-    ProxyHttpsController() = default;
-    ~ProxyHttpsController() = default;
-    /**
-    * @brief Returns map of headers for a given HTTP request.
-    * @param stream Remainder of HTTP stream.
-    * @return Map of headers.
-    */
-    static std::map<std::string, std::string> parseHeaders(
-        std::istringstream &stream);
+ public:
+  ProxyHttpsController() = default;
+  ~ProxyHttpsController() = default;
+  std::string getFigureByName(const std::string &name);
 
-    /**
-    * @brief Returns a map of parts for an HTTP request.
-    * @param request HTTP request to build map for.
-    * @return Map of HTTP request.
-    */
-    static std::map<std::string, std::map<std::string, std::string>>
-    parseHttpRequest(const std::string &request);
-
-    /**
-    * @brief Parses last part of a URL for resource.
-    * @param url URL to parse.
-    * @return Last portion of URL.
-    */
-    static std::string getLastPath(const std::string &url);
-
-    /**
-    * @brief Confirms whether passed URL is pointing to desired section.
-    * @param url URL to confirm.
-    * @return Whether URL points to section or not.
-    */
-    static bool validateUrlFormat(const std::string &url);
-
-    /**
-    * @brief Builds an HTTP response.
-    * @param statusCode Status code for response.
-    * @param body Body for response.
-    * @return Response.
-    */
-    static std::string generateHttpResponse(int statusCode,
-                                            const std::string &body);
-    /**
-    * @brief Builds and sends an HTTP response over a socket.
-    * @param client Socket to send response through.
-    * @param statusCode Status code for response.
-    * @param body Body for response.
-    */
-    static void sendHttpResponse(const std::shared_ptr<IPv4SslSocket> &client,
-                                int statusCode, const std::string &body);
-
-    /**
-    * @brief Builds and sends an HTTPS response over a socket.
-    * @param client Socket to send response through.
-    * @param statusCode Status code for response.
-    * @param body Body for response.
-    */
-    static void sendHttpsResponse(const std::shared_ptr<IPv4SslSocket> &client,
-                                  int statusCode, const std::string &body);
-    /**
-    * @brief serves the response for the usual clients (web browser and lego
-    * clients)
-    * @param client socket to send the response
-    * @param request request from the client
-    */
-    void serveFigure(const std::shared_ptr<IPv4SslSocket> &client,
-                      const std::string &request);
+ private:
+  // instancia routing table
 };
--- lego-proxy/main/include/net/ProxyHttpsServer.hpp
+++ lego-proxy/main/include/net/ProxyHttpsServer.hpp
@@ -3,25 +3,84 @@
 #include "../../../../lego-common/main/include/net/SslServer.hpp"
 #include "../controller/ProxyHttpsController.hpp"
 
-
-
-
 class ProxyHttpsServer : public SslServer {
-  public:
-    ProxyHttpsServer(int32_t numWorkers, const std::string& certPath,
-                              int32_t port): SslServer(numWorkers, certPath, port) {}
-    
-    ~ProxyHttpsServer() {}
-    
-    /** receive a client to handle. This client is a socket that has already
-     * ben ssl accepted.
-     * 
-    */
-    void handleClient(const std::shared_ptr<IPv4SslSocket>& client) override;
-    
+ public:
+  ProxyHttpsServer(int32_t numWorkers, const std::string &certPath,
+                   int32_t port)
+      : SslServer(numWorkers, certPath, port) {}
 
-  private:
-    ProxyHttpsController proxyHttpsController{};
-    void handleAcceptError(const std::exception& e) override;
+  ~ProxyHttpsServer() {}
 
+  /** receive a client to handle. This client is a socket that has already
+   * ben ssl accepted.
+   *
+   */
+  void handleClient(const std::shared_ptr<IPv4SslSocket> &client) override;
+
+ private:
+  ProxyHttpsController proxyHttpsController{};
+  void handleAcceptError(const std::exception &e) override;
+  /**
+   * @brief Returns map of headers for a given HTTP request.
+   * @param stream Remainder of HTTP stream.
+   * @return Map of headers.
+   */
+  static std::map<std::string, std::string> parseHeaders(
+      std::istringstream &stream);
+
+  /**
+   * @brief Returns a map of parts for an HTTP request.
+   * @param request HTTP request to build map for.
+   * @return Map of HTTP request.
+   */
+  static std::map<std::string, std::map<std::string, std::string>>
+  parseHttpRequest(const std::string &request);
+
+  /**
+   * @brief Parses last part of a URL for resource.
+   * @param url URL to parse.
+   * @return Last portion of URL.
+   */
+  static std::string getLastPath(const std::string &url);
+
+  /**
+   * @brief Confirms whether passed URL is pointing to desired section.
+   * @param url URL to confirm.
+   * @return Whether URL points to section or not.
+   */
+  static bool validateUrlFormat(const std::string &url);
+
+  /**
+   * @brief Builds an HTTP response.
+   * @param statusCode Status code for response.
+   * @param body Body for response.
+   * @return Response.
+   */
+  static std::string generateHttpResponse(int statusCode,
+                                          const std::string &body);
+  /**
+   * @brief Builds and sends an HTTP response over a socket.
+   * @param client Socket to send response through.
+   * @param statusCode Status code for response.
+   * @param body Body for response.
+   */
+  static void sendHttpResponse(const std::shared_ptr<IPv4SslSocket> &client,
+                               int statusCode, const std::string &body);
+
+  /**
+   * @brief Builds and sends an HTTPS response over a socket.
+   * @param client Socket to send response through.
+   * @param statusCode Status code for response.
+   * @param body Body for response.
+   */
+  static void sendHttpsResponse(const std::shared_ptr<IPv4SslSocket> &client,
+                                int statusCode, const std::string &body);
+  /**
+   * @brief serves the response for the usual clients (web browser and lego
+   * clients)
+   * @param client socket to send the response
+   * @param request request from the client
+   */
+  void serveFigure(const std::shared_ptr<IPv4SslSocket> &client,
+                   const std::string &request);
 };
